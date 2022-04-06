d9f05501f7121c057ec87ddf8ca5af499409c894
Maarten Rozendale <m@rozendale.com>
2022-04-06 15:57:48 +0700

    done

--- src/components/events/event-form.js
+++ src/components/events/event-form.js
@@ -1,14 +1,17 @@
 import { useLocation } from 'react-router-dom';
+import { CssTextField } from '../layout/element';
 
 
 export default function EventForm(){
 
-    const {state} = useLocation();
-    const {group} = state;
-    console.log(state)
+    // const {state} = useLocation();
+    // const {group} = state;
+    // console.log(state)
 
     return (
-        <h1>New Event for the group {state && state.id}</h1>
-
+        <div>
+            <CssTextField label="Team 1" />
+            <h1>New Event for the group</h1>
+        </div>
     )
 }
---
+++ src/components/layout/element.js
@@ -0,0 +1,24 @@
+import { withStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+
+export const CssTextField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: 'green',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: 'green',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'red',
+            },
+            '&:hover fieldset': {
+                borderColor: 'yellow',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'green',
+            },
+        }
+    }
+})(TextField);
